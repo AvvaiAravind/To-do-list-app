0281e6efffe77b4ae253113f7d91426d4efbc6b9
Avvai Aravind <avvaiaravind@gmail.com>
2024-10-17 12:37:53 +0530

    added all the file required to start the project

---
+++ src/index.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const content = document.querySelector("div");
+  content.textContent = "Hello, World!";
+  document.body.appendChild(content);
+});
